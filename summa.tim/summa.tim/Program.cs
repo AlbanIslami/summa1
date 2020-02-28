c58f0fb0261b982c60ce6bfe919a82d42216011b
Elev <Elev@TEB127022>
2020-02-28 13:50:23 +0100

    klar med kod

---
+++ summa.tim/summa.tim/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+
+namespace summa.txt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            int fem = 0;
+            try
+            {
+                using (BinaryReader br = new BinaryReader(new FileStream("talfil.tf", FileMode.OpenOrCreate, FileAccess.Read)))
+                {
+                    
+                    for (int i = 0; i < 1000000; i++) 
+                    {
+                        int tal = br.ReadInt32();
+                        
+                        sum = sum + tal;
+                        if(tal == 5)
+                        {
+                            fem++;
+                        }
+                        else
+                        {
+
+                        }                        
+                    }
+
+                    br.Close();
+
+                }
+            }
+            catch
+            {
+                Console.WriteLine("fel");
+            }
+
+            Console.WriteLine(sum);
+            Console.WriteLine(fem);
+
+            StreamWriter sw = new StreamWriter("summa.txt");
+
+
+            
+            
+        }
+    }
+}
